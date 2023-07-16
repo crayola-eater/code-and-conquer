04b688366871242d937ab8a3ac2763991dfbc8ab
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-07-16 17:48:58 +0100

    Adding structs for defend command

---
+++ src/core/command/defend.rs
@@ -0,0 +1,14 @@
+use crate::types::{GridSquare, SenderDetails};
+
+#[derive(Debug)]
+pub struct DefendRequest {
+  pub game_id: i32,
+  pub sender: SenderDetails,
+  pub row_index: i32,
+  pub column_index: i32,
+}
+
+#[derive(Debug)]
+pub struct DefendResponse {
+  pub square: GridSquare,
+}
